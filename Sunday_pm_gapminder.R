48cbf7378d5fec33f0bafc118f91b47b0d167b92
malini <malinikotak@yahoo.com>
2015-04-19 13:36:08 -0500

    Add line to read data

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -1,3 +1,7 @@
 # This is a script to explore the gapminder dataset using R
 # Malini
 # 4/19/15
+
+# Read in data as tab-delimited
+
+gap.in <- read.table("output/combined_gapMinder.tsv", sep = "\t")
